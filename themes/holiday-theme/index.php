b6c63f77f1f6215c32af559e53821e7256e37e65
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-09-09 18:20:17 +0330

    add comment if js is disable

--- themes/holiday-theme/index.php
+++ themes/holiday-theme/index.php
@@ -20,6 +20,20 @@
 	<link rel="stylesheet" type="text/css" href="style.css" media="all" />
 </head>
 <body>
-	<div class="wrapper"></div>
+	<div class="wrapper">
+		<noscript>
+        	<div class="global-site-notice noscript">
+	            <div class="notice-inner">
+	                <p class="medium">
+	                    <strong>به نظر می رسد که جاوا اسکریپت در مرورگر شما غیر فعال می باشد.</strong><br />
+	                    شما باید برای استفاده از قابلیتهای این وب سایت جاوا اسکریپت را در مرورگر خود فعال کنید.
+	                </p>
+	            </div>
+	            <div class="page">
+	            	
+	            </div>
+	        </div>
+  		</noscript>
+	</div>
 </body>
 </html>
