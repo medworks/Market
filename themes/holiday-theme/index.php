02f6131133491a367244fecacdf879c0e0dd7d6a
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-09-09 12:36:46 +0330

    add basic codes

---
+++ themes/holiday-theme/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html lang="fa">
+<head>
+	<title></title>
+
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=IE8">
+	<meta charset="UTF-8">
+	<meta name="robots" content="INDEX,FOLLOW">
+	<meta name="description" content="market">
+	<meta name="keywords" content="market,cms,mediateq">
+	<meta http-equiv="Content-Language" content="Fa">
+	<meta http-equiv="Designer" content="مدیاتک">
+	<meta name="Generator" content="مدیاتک">
+	<meta name="Author" content="مدیاتک">
+
+	<link rel="icon" href="favicon.ico" type="image/x-icon"/>
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+
+
+
+</head>
+<body>
+	
+</body>
+</html>
