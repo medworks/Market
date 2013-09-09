ee4ddb7ef705537e202c8ceba034caee1a6d7001
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-09-09 18:09:23 +0330

    add style to project

--- themes/holiday-theme/index.php
+++ themes/holiday-theme/index.php
@@ -3,6 +3,7 @@
 <head>
 	<title></title>
 
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=IE8">
 	<meta charset="UTF-8">
 	<meta name="robots" content="INDEX,FOLLOW">
@@ -13,13 +14,12 @@
 	<meta name="Generator" content="مدیاتک">
 	<meta name="Author" content="مدیاتک">
 
-	<link rel="icon" href="favicon.ico" type="image/x-icon"/>
-	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+	<link rel="icon" href="favicon.ico" type="image/x-icon" />
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
 
-
-
+	<link rel="stylesheet" type="text/css" href="style.css" media="all" />
 </head>
 <body>
-	
+	<div class="wrapper"></div>
 </body>
 </html>
